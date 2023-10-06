d16390ecf48577180736372389f667ed2aa23536
peaknicc <ybkz17@naver.com>
2023-10-06 18:17:23 +0900

    init

---
+++ src/test/java/com/ohgiraffers/mapping/JpaMappingAnnotationApplicationTests.java
@@ -0,0 +1,13 @@
+package com.ohgiraffers.mapping;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class JpaMappingAnnotationApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
